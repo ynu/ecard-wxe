aa5c6812b5da7b362ee31190af7c46c2bd42db2d
na57 <na57@qq.com>
2016-11-16 16:04:39 +0800

    update

--- src/components/Weui/panel/panel.js
+++ src/components/Weui/panel/panel.js
@@ -19,8 +19,8 @@ export default class Panel extends React.Component {
     render() {
         const {children, className, access, ...others} = this.props;
         const cls = classNames({
-            weui_panel: true,
-            weui_panel_access: access,
+            'weui-panel': true,
+            'weui-panel_access': access,
             [className]: className
         });
 
