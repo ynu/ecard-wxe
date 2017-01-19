bda19855c21aaf57fc24f67e5bee6f7e383fe2c4
na57 <na57@qq.com>
2017-01-19 09:37:16 +0800

    add：完成商户树形结构权限检查

--- src/api/controllers/shop.js
+++ src/api/controllers/shop.js
@@ -5,32 +5,9 @@ import { SUCCESS, SERVER_FAILED, OBJECT_IS_NOT_FOUND, UNAUTHORIZED } from 'nagu-
 import { auth, wxeapi, getShopTag, error, info, mysqlUrl } from '../../config';
 import * as wxeAuth from '../controllers/wxe-auth-middlewares';
 
+const getTagList = () => wxeapi.getTagList();
 const router = new Router();
 
-const canUserReadShopBill = async (userId, shop, shops) => {
-  // 1. 获取商户的所有祖先节点；
-  const getAncestors = shop2 => {
-    if (shop2.shopId === '1') return [];
-    // 当前节点的父节点
-    const father = shops.find(s => s.shopId === shop2.fShopId);
-    return [
-      ...getAncestors(father),
-      father,
-    ];
-  };
-  const ancestors = [
-    ...getAncestors(shop),
-    shop,
-  ];
-  info('ancestors of the shop:', ancestors.map(s => s.shopId));
-  // 2. 根据所有祖先节点获取tags；
-  const tagnames = ancestors.map(s => s.shopName);
-  info('tag names of ancestors:', tagnames);
-
-  // 3. 获取所有tags对应的用户列表；
-
-  // 4. 判断用户是否在用户列表中。
-};
 router.get('/:shopId/daily-bill/:accDate',
   // 确保用户已登录
   expressJwt({
@@ -38,42 +15,62 @@ router.get('/:shopId/daily-bill/:accDate',
     credentialsRequired: true,
     getToken: wxeAuth.getToken,
   }),
-  // async (req, res, next) => {
-  //   const { shopId, accDate } = req.params;
-  //   const shopBill = await yktManager.getShopBill(shopId, accDate);
-  //   const shopBills = await yktManager.getShopBills(null, accDate);
-  //   canUserReadShopBill(req.user.UserId, shopBill, shopBills);
-  //   next();
-  // },
-  // 确保当前用户对当前的shop有权限
+  // 检查当前用户是否有权限
   async (req, res, next) => {
-    const { shopId, accDate } = req.params;
-    // 0. 取出当前账单
     try {
+      const { shopId, accDate } = req.params;
+      const userid = req.user.UserId;
       const yktManager = new YktManager({ url: mysqlUrl });
-      const shopBill = await yktManager.getShopBill(shopId, accDate);
-      if (!shopBill) return res.send({ ret: OBJECT_IS_NOT_FOUND });
-      res.shopBill = shopBill;
-      // 1. 取系统所有tag列表
-      const tags = await wxeapi.getTagList();
 
-      // 2. 筛选出当前shop对应的tag
-      let tag = tags.find(t => t.tagname === getShopTag(shopBill.shopName));
-      if (!tag) return res.send({ ret: OBJECT_IS_NOT_FOUND });
-      // 3. 检查此tag中是否包含当前用户
-      tag = await wxeapi.getTag(tag.tagid);
+      // 0. 取当前商户账单
+      res.shopBill = await yktManager.getShopBill(shopId, accDate);
+      if (!res.shopBill) {
+        error('读取当前商户账单失败, shopId:', shopId, 'accDate:', accDate);
+        res.send({ ret: OBJECT_IS_NOT_FOUND });
+        return;
+      }
+      // 1. 获取商户的所有祖先节点；
+      const ancestors = await yktManager.getAncestorShops(shopId);
+      info('ancestors of the shop:', ancestors.map(s => s.shopId));
 
+      // 2. 根据当前商户及所有祖先节点获取tags；
+      const tagnames = [
+        ...ancestors.map(s => s.shopName),
+        res.shopBill.shopName,
+      ].map(sname => getShopTag(sname));
+      info('tag names of ancestors:', tagnames);
 
-      info('current user:', req.user);
-      if (tag.userlist.find(user => user.userid === req.user.UserId)) {
+      // 3. 获取所有tags对应的用户列表；
+      // 3.1 获取所有tag列表
+      const taglist = await getTagList();
+      // 3.2 获取所有tag的详细信息
+      const tagPromises = tagnames.map(tagname => {
+        const tag = taglist.find(t => t.tagname === tagname);
+        if (tag) return wxeapi.getTag(tag.tagid);
+        return null;
+      }).filter(p => p !== null);
+      info('tagPromises count:', tagPromises.length);
+
+      const tagDetails = await Promise.all(tagPromises);
+      // 3.3 获取所有tag对应的所有用户
+      const users = tagDetails.map(td => td.userlist)
+        .reduce((userlist, cur) => userlist.concat(cur), []);
+      info('users count:', users.length);
+      // 4. 判断用户是否在用户列表中。
+      if (users.some(u => u.userid === userid)) {
+        info('用户权限检查通过');
         next();
       } else {
+        info('用户权限检查失败');
         res.send({ ret: UNAUTHORIZED });
       }
-    } catch (msg) {
-      res.send({ ret: SERVER_FAILED, msg: msg.message });
+    } catch (e) {
+      error('检查当前用户是否有权限错误:', e.message);
+      error('检查当前用户是否有权限错误:', e.stack);
+      res.send({ ret: SERVER_FAILED, msg: e });
     }
   },
+  // 发送账单数据
   async (req, res) => {
     const { shopId, accDate } = req.params;
     try {
