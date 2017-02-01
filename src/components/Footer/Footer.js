451708f14592f393ed424566d6c2513e6256d456
na57 <na57@qq.com>
2017-02-01 15:44:35 +0800

    页脚加上API链接

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => (
   <div className="weui-footer" style={{ marginTop: '10px' }}>
     <p className="weui-footer__links">
       <a href="http://www.itc.ynu.edu.cn" className="weui-footer__link">信息技术中心</a>
+      <a href="http://go.ynu.edu.cn/ecard-api" className="weui-footer__link">API</a>
     </p>
     <p className="weui-footer__text">Copyright © 2016-{(new Date()).getFullYear()} 云南大学</p>
   </div>
