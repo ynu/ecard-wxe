aa3154ebc3429009ae5e682e53cb316582e35273
na57 <na57@qq.com>
2017-02-01 16:16:28 +0800

    修正合计金额计算方式

--- src/routes/daily-report/DeviceBill.js
+++ src/routes/daily-report/DeviceBill.js
@@ -2,16 +2,27 @@ import React, { PropTypes } from 'react';
 import { Cell, CellBody, CellFooter, Badge } from 'react-weui';
 import { formatMoney, formatNumber } from 'accounting';
 
-const DeviceBill = ({ bill: { transCnt, crAmt, deviceName } }) => (
-  <Cell>
-    <CellBody>
-      {deviceName}
-      <Badge preset="body">{formatNumber(transCnt)}笔</Badge>
-    </CellBody>
-    <CellFooter>
-      {formatMoney(crAmt, '￥')}
-    </CellFooter>
-  </Cell>
+const DeviceBill = ({ bill: { transCnt, crAmt, drAmt, deviceName } }) => {
+  const amount = parseFloat(crAmt, 10) - parseFloat(drAmt, 10);
+  return (
+    <Cell>
+      <CellBody>
+        {deviceName}
+        <Badge preset="body">{formatNumber(transCnt)}笔</Badge>
+      </CellBody>
+      <CellFooter>
+        {formatMoney(amount, '￥')}
+      </CellFooter>
+    </Cell>
   );
+};
 
+DeviceBill.propTypes = {
+  bill: PropTypes.shape({
+    crAmt: PropTypes.string.isRequired,
+    drAmt: PropTypes.string.isRequired,
+    transCnt: PropTypes.string.isRequired,
+    deviceName: PropTypes.string.isRequired,
+  }).isRequired,
+};
 export default DeviceBill;
