b15007919a7a8af730ec29d88487e31d4246e159
na57 <na57@qq.com>
2017-02-21 12:12:41 +0800

    移除对YktManager的依赖

--- src/api/models/jobs.js
+++ src/api/models/jobs.js
@@ -2,16 +2,24 @@
 
 import { scheduleJob } from 'node-schedule';
 import moment from 'moment';
-import { YktManager } from 'ecard-api';
 import cache from 'memory-cache';
 import { roles, wxeapi, dailyReportCron,
-  auth, getTag, mysqlUrl, dailyReportPicUrl, host,
-  error, info, TAG_LIST, CACHE_TIME_10_DAYS,
-  getShopBillCacheKey, getShopAncestorsCacheKey,
+  auth, getTag, dailyReportPicUrl, host,
+  error, info, TAG_LIST,
+  getShopBillCacheKey,
   getShopDailyBillsCacheKey, getDeviceBillsCacheKey,
   getSubShopDailyBillsCacheKey } from '../../config';
 import * as shopModel from './cachedShop';
 
+const TEN_DAYS = 10 * 24 * 60 * 60 * 1000;
+
+/*
+发送账单给微信用户
+参数：
+  - bill 账单对象
+  - to 接收账单的用户对象，例如: { touser: ['na57'] }
+  - agentId 整数，用于发送消息的企业号应用Id
+ */
 const sendBill = async (bill, to, agentId) => {
   try {
     // 生成消息文本
@@ -35,26 +43,26 @@ const cacheDailyData = async day => {
   // 1. 缓存指定日期的所有日账单
   const shopBills = await shopModel.fetchShopDailyBills(day, {
     key: getShopDailyBillsCacheKey(day),
-    expire: CACHE_TIME_10_DAYS,
+    expire: TEN_DAYS,
   });
   // 2. 缓存每个商户的所有数据
   const billPromises = shopBills.map(bill => {
     // 2.1. 缓存每一个商户的日账单
     const pBill = shopModel.fetchDailyBill(bill.shopId, day, {
       key: getShopBillCacheKey(bill.shopId, day),
-      expire: CACHE_TIME_10_DAYS,
+      expire: TEN_DAYS,
     });
 
     // 2.2. 缓存每个商户的子商户日账单列表
     const pSubShopBills = shopModel.fetchSubShopDailyBills(bill.shopId, day, {
       key: getSubShopDailyBillsCacheKey(bill.shopId, day),
-      expire: CACHE_TIME_10_DAYS,
+      expire: TEN_DAYS,
     });
 
     // 2.3. 缓存每个商户的设备日账单列表
     const pDeviceBills = shopModel.fetchDeviceDailyBills(bill.shopId, day, {
       key: getDeviceBillsCacheKey(bill.shopId, day),
-      expire: CACHE_TIME_10_DAYS,
+      expire: TEN_DAYS,
     });
     return Promise.all([pBill, pSubShopBills, pDeviceBills]);
   });
@@ -65,15 +73,15 @@ const reportDailyShopBill = async () => {
   try {
     // 0. 从远程获取tag列表，并缓存
     const tags = await wxeapi.getTagList();
-    cache.put(TAG_LIST, tags, CACHE_TIME_10_DAYS);
+    cache.put(TAG_LIST, tags, TEN_DAYS);
     info('tags count: ', tags.length);
 
     const yestoday = moment().subtract(1, 'days').format('YYYYMMDD');
     // 1. 获取商户账单列表
-    info('connect to:', mysqlUrl);
-    const yktManager = new YktManager({ url: mysqlUrl });
-    const shopBills = await yktManager.getShopBills(null, yestoday);
-    info('shop bills count: ', shopBills.length);
+    const shopBills = await shopModel.fetchShopDailyBills(yestoday, {
+      key: getShopDailyBillsCacheKey(yestoday),
+      expire: TEN_DAYS,
+    });
 
     // 2. 循环每个商户账单
     const result = shopBills.map(bill => {
